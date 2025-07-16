c493cddc66edb6adb82caa87eecada2d74ca8fde
Ahmed Nadeem <ahmedghtwhts786@gmail.com>
2025-07-16 20:14:45 +0500

    init

---
+++ themes/theme.ts
@@ -0,0 +1,9 @@
+import type { ThemeConfig } from "antd/es/config-provider/context";
+
+export const themeConfig: ThemeConfig = {
+  token: {
+    colorPrimary: "#1890ff",
+    borderRadius: 4,
+    fontFamily: "Inter, sans-serif",
+  },
+};
